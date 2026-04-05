583b1f6ab3c155cd34307d2f05215ad68673900f
Jacek <jacek.soroko@me.com>
2026-04-05 23:15:11 +0100

    Completed Section 35

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,18 @@
 import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "@/app/_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 // export const revalidate = 15;
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +26,12 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
